6934be89e1a1db8048ba62f87049aea70bb8e6a1
macenkk <macenkk@gmail.com>
2025-05-02 15:32:51 +0300

    Refactor: English version of IP injection script for Terraform

--- Fourth_Part/Task_1/command.sh
+++ Fourth_Part/Task_1/command.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-# JSON dosyasından IP'yi al
+# Extract the private IP address from the JSON file
 IP=$(jq -r '.Instances[0].PrivateIpAddress' info.json)
 
-# terraform.tf içinde 40. satırdaki ec2_private_ip satırını değiştir
+# Replace line 40 in terraform.tf with the updated IP value
 sed -i "40s/.*/  ec2_private_ip = \"${IP}\"/" terraform.tf
 
-# Bilgi mesajı
-echo "IP güncellendi: ${IP}"
+# Display confirmation
+echo "Private IP has been updated in terraform.tf: ${IP}"
